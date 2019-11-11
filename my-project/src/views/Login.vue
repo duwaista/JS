51e4c64bab39b93d2aea8f12d3aa8775bce5028e
Vlad <vlad.45.45@mail.ru>
2019-11-11 23:51:34 +0700

    Add Firebase

--- my-project/src/views/Login.vue
+++ my-project/src/views/Login.vue
@@ -1,67 +1,45 @@
 <template>
-  <v-row justify="center">
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline">User Profile</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal first name*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Email*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+<div>
+  <!--h2>{{ location.image }}</h2-->
+  <article v-for="(location, idx) in locations" :key="idx">
+    <img :src="location.image">
+    <h1>{{location.name}}</h1>
+    <v-btn @click="deleteLocation(location.id)">Delete</v-btn>
+  </article>
+
+<form @submit="addLocation(name, image)">
+<input v-model="name" placeholder="Location Name">
+<input v-model="image" placeholder="Location Image URL">
+<v-btn type="submit">Add New Location</v-btn>
+</form>
+
+</div>
 </template>
+
 <script>
+  import {db} from '@/main'
   export default {
-    data: () => ({
-      dialog: false,
-    }),
+    name: 'Login',
+    data () {
+      return{
+        locations: [],
+        name: '',
+        image: '',
+      }
+    },
+    firestore(){
+      return {
+        locations: db.collections('locations').orderBy('createdAt')
+      } 
+    },
+    methods:{
+      addLocation(name, image) {
+        const createdAt = new Date()
+        db.collection('locations').add({name, image, createdAt})
+      },
+      deleteLocation(id) {
+        db.collection('locations').doc(id).delete()
+      }
+    }
   }
 </script>
