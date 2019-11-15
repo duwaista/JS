1ac87d9d9ec111440d3785ae4fa721cfefa4add3
EgorovVI <EgorovVI@DOM201.LOCAL>
2019-11-15 16:23:26 +0700

    Firebase auth

--- my-project/src/views/Authentication.vue
+++ my-project/src/views/Authentication.vue
@@ -1,6 +1,36 @@
 <template>
-  <h1>Hello</h1>
+<div align="center">
+<input v-model="email" type="email" placeholder="Login">
+<input v-model="password" type="password" placeholder="Password">
+<v-btn type="submit">Add user</v-btn>
+<div>
+
+</div>
+</div>
 </template>
+
 <script>
-  export default {}
+  import {db} from '@/main'
+import { error } from 'util'
+  export default {
+    name: 'Login',
+    data () {
+      return{
+        user: {
+          email: '',
+          password: ''
+        }
+       
+      }
+    },
+
+    methods:{
+      registerUser() {
+      firebase.auth().createUserWithEmailAndPassword(this.user.email, this.user.password)
+      .catch( error =>{
+        console.log(error)
+      })
+      }
+    }
+  }
 </script>
